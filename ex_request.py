dcf84bb823b689786f643caf96bd313ecdb1975a
jjy-cc <yjy0yjy@gmail.com>
2024-07-24 17:16:29 +0900

    requests ex2

--- ex_request.py
+++ ex_request.py
@@ -1,5 +1,12 @@
 import requests
 
-target = "https://jsonplaceholder.typicode.com/users/1"
+target = "https://jsonplaceholder.typicode.com/users"
 response = requests.get(url = target)
-print(response.text)
+
+data = response.json()
+
+name_list = []
+for user in data:
+    name_list.append(user['name'])
+
+print(name_list)
