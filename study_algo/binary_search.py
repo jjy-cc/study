f39bd75f925a75de6502f0117935f7ae8efa0d99
jjy-cc <yjy0yjy@gmail.com>
2024-07-24 22:08:49 +0900

    이진 알고리즘

---
+++ study_algo/binary_search.py
@@ -0,0 +1,20 @@
+def binary_search(array, start, end, target):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, start, mid - 1, target)
+    else:
+        return binary_search(array, mid + 1, end, target)
+
+def binary_search2(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
